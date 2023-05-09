7f494e35363a4c2f7a21f5f1277ad47324893c48
Abdelrhman111111 <Abdelrhmanmohamed.m14@gmail.com>
2023-05-09 08:31:37 -0700

    done

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
 for l in range(97, 123):
-    if chr(l) != 'q' and chr(l) != 'e':
-        print("{}".format(chr(l)), end="")
+    print("{}".format(chr(l)), end="")
