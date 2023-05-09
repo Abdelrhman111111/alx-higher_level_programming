31b31725042c9763eb6c798da53e201a7361157e
Abdelrhman111111 <Abdelrhmanmohamed.m14@gmail.com>
2023-05-09 07:41:58 -0700

    done

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - ................
+ * Return: .............
+ */
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n",i);
+	else
+		printf("%i is zero\n", i);
+
+	return (0);
+}
