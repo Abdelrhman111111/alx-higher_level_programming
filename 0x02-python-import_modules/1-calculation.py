51b2847c3680d43c5b3018c03096cf65f72f69a3
Abdelrhman111111 <Abdelrhmanmohamed.m14@gmail.com>
2023-05-11 15:31:28 -0700

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import a, s, d, m
-    x = 10
-    y = 5
-    print("{} + {} = {}".format(x, y, a(x, y)))
-    print("{} - {} = {}".format(x, y, s(x, y)))
-    print("{} * {} = {}".format(x, y, m(x, y)))
-    print("{} / {} = {}".format(x, y, d(x, y)))
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, a(a, b)))
+    print("{} - {} = {}".format(a, b, s(a, b)))
+    print("{} * {} = {}".format(a, b, m(a, b)))
+    print("{} / {} = {}".format(a, b, d(a, b)))
