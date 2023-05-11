04af80d49c96562948d6d86da666a2b4abfd1b6e
Abdelrhman111111 <Abdelrhmanmohamed.m14@gmail.com>
2023-05-11 15:33:45 -0700

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    vars = dir(hidden_4)
+    xs = dir(hidden_4)
     for x in xs:
         if x[:2] != "__":
             print(x)
