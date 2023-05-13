26df9e1c65d7cafd67994be158cca7f979c78d19
Abdelrhman111111 <122821593+Abdelrhman111111@users.noreply.github.com>
2023-05-13 16:08:10 +0300

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/10-divisible_by_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    n = []
-    for x in my_list:
-        if x % 2 == 0:
-            n.append(True)
-        else:
-            n.append(False)
-    return n
