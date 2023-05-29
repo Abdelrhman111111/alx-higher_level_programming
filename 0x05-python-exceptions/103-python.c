409926cb87fee041b55de4ca94a8ed12493ee761
Abdelrhman111111 <Abdelrhmanmohamed.m14@gmail.com>
2023-05-29 08:36:17 -0700

    done

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -55,26 +55,26 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size = 0;
-	PyObject *item;
-	int i = 0;
+	Py_ssize_t s = 0;
+	PyObject *i;
+	int n = 0;
 
 	fflush(stdout);
 	printf("[*] Python list info\n");
 	if (PyList_CheckExact(p))
 	{
-		size = PyList_GET_SIZE(p);
-		printf("[*] Size of the Python List = %zd\n", size);
+		s = PyList_GET_SIZE(p);
+		printf("[*] Size of the Python List = %zd\n", s);
 		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-		while (i < size)
+		while (n < s)
 		{
-			item = PyList_GET_ITEM(p, i);
-			printf("Element %d: %s\n", i, item->ob_type->tp_name);
-			if (PyBytes_Check(item))
-				print_python_bytes(item);
-			else if (PyFloat_Check(item))
-				print_python_float(item);
-			i++;
+			i = PyList_GET_ITEM(p, n);
+			printf("Element %d: %s\n", n, i->ob_type->tp_name);
+			if (PyBytes_Check(i))
+				print_python_bytes(i);
+			else if (PyFloat_Check(i))
+				print_python_float(i);
+			n++;
 		}
 	}
 	else
